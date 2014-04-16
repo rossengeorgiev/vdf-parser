2a95f5d0d51681d06e436969ec2b598fd40bf814
Rossen Georgiev <zoorty@gmail.com>
2014-04-16 19:42:36 +0100

    updated vdf.py
    * added exceptions when pasing incorrect type to function
    * added dump(), turns a dict into VDF
    * added test(), a few simple test for parse() and drump()
    
    NOTE: parse() does not validate VDF format.
    Invalid VDF will produce unexpected results

--- vdf.py
+++ vdf.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# a simple parse for Valve's KeyValue format
+# a simple parser for Valve's KeyValue format
 # https://developer.valvesoftware.com/wiki/KeyValues
 #
 # author: Rossen Popov, 2014
@@ -9,11 +9,25 @@
 
 import re
 
-####################################
-# takes file and returns a dict
-####################################
+###############################################
+#
+# Takes a file or str and returns dict
+#
+# Function assumes valid VDF as input.
+# Invalid VDF will result in unexpected output
+#
+###############################################
 
-def parse(fileobj):
+def parse(a):
+    a_type = type(a)
+
+    if a_type is file:
+        lines = a.readlines()
+    elif a_type is str:
+        lines = a.split('\n')
+    else:
+        raise ValueError("Expected parametar to be file or str")
+
     obj = dict()
     stack = [obj]
     skipnext = False
@@ -21,7 +35,7 @@ def parse(fileobj):
 
     re_keyvalue = re.compile('^"((?:[^"\\\]|\.)*)"[ \t]+"((?:[^"\\\]|\.)*)"')
 
-    for line in fileobj.readlines():
+    for line in lines:
         line = line.strip()
 
         # skip empty and comment lines
@@ -55,3 +69,83 @@ def parse(fileobj):
                 skipnext = True
 
     return obj
+
+###############################################
+#
+# Take a dict, reuturns VDF in str buffer
+#
+# dump(dict(), pretty=True) for indented VDF
+#
+###############################################
+
+def dump(a, **kwargs):
+    pretty = kwargs.get("pretty", False)
+
+    if type(pretty) is not bool:
+        raise ValueError("Pretty option is a boolean")
+
+    return _dump(a,pretty)
+
+def _dump(a,pretty=False,level=0):
+    indent = "\t"
+
+    if type(a) is not dict:
+        raise ValueError("Expected parametar to be dict")
+
+    buf = ""
+    line_indent = ""
+
+    if pretty:
+        line_indent = indent * level
+
+    for key in a:
+        if type(a[key]) is dict:
+            buf += '%s"%s"\n%s{\n%s%s}\n' % (line_indent, key, line_indent, _dump(a[key],pretty,level+1), line_indent)
+        else:
+            buf += '%s"%s" "%s"\n' % (line_indent, key, str(a[key]))
+
+    return buf
+
+###############################################
+#
+# Testing initiative
+#
+###############################################
+
+
+def test():
+    tests = [
+                [ '' , {} ],
+                [ {} ,  '' ],
+                [ "//comment text\n//comment", {} ],
+                [ {1:1}, '"1" "1"\n'],
+                [ {"a":"1","b":"2"} , '"a" "1"\n"b" "2"\n' ],
+                [ '//comment\n"a" "1"\n"b" "2" //comment' , {"a":"1","b":"2"} ],
+                [ {"a":{"b":{"c":{"d":"1","e":"2"}}}} , '"a"\n{\n"b"\n{\n"c"\n{\n"e" "2"\n"d" "1"\n}\n}\n}\n' ],
+                [ '"a"\n{\n"b"\n{\n"c"\n{\n"e" "2"\n"d" "1"\n}\n}\n}\n' , {"a":{"b":{"c":{"d":"1","e":"2"}}}} ],
+            ]
+
+    for test,expected in tests:
+        out = None
+
+        try:
+            if type(test) is dict:
+                out = dump(test)
+            else:
+                out = parse(test)
+        except:
+            print "Test falure (exception):\n\n%s" % str(test)
+            raise
+
+        if cmp(expected,out) != 0:
+            print "Test falure (ouput mismatch):\n\n%s" % str(test)
+            print "\nOutput:\n\n%s" % str(out)
+            print "\nExpected:\n\n%s\n" % str(expected)
+
+            raise Exception("Output differs from expected result")
+
+    return True
+
+
+
+
