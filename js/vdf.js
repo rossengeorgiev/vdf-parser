17996e1982cfaee94a93bae6d858ec18da23236c
Rossen Georgiev <rossen@rgp.io>
2016-10-12 18:11:05 +0300

    merge duplicate kv list with diplicate key

--- js/vdf.js
+++ js/vdf.js
@@ -64,7 +64,10 @@ var VDF = {
                 var val = (m[6] !== undefined) ? m[6] : m[8];
 
                 if(val === undefined) {
-                    stack[stack.length-1][key] = {};
+                    // chain (merge) duplicate key
+                    if(stack[stack.length-1][key] === undefined)
+                        stack[stack.length-1][key] = {};
+
                     stack.push(stack[stack.length-1][key]);
                     expect_bracket = true;
                 }
